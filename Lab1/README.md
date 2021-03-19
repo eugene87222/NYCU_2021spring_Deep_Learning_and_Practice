c6dbf2bbb4765c66ab0131989e5ebd42f0725a4c
Eugene Yang <eugene87222@gmail.com>
2021-03-19 19:39:37 +0800

    add readme

---
+++ Lab1/README.md
@@ -0,0 +1,47 @@
+# Lab1: Backpropagation
+
+## Report Spec
+
+1. Introduction (20%)
+2. Experiment setups (30%)
+    - Sigmoid functions
+    - Neural network
+    - Backpropagation
+3. Results of your testing (20%)
+    - Screenshot and comparison figure
+    - Show the accuracy of your prediction
+    - Learning curve (loss, epoch curve)
+    - Anything you want to present
+4. Discussion (30%)
+    - Try different learning rates
+    - Try different numbers of hidden units
+    - Try without activation functions
+    - Anything you want to share
+5. Extra (10%)
+    - Implement different optimizers (2)
+    - Implement different activation functions (3)
+    - Implement convolutional layers (5)
+
+## Score
+
+- 60% demos core (experimental results& questions)
+- 40% report
+- For experimental results, you have to achieve at least 90% of accuracy to get the demo score.
+- If the zip file name or the report spec have format error, you will be punished. (-5)
+
+## To do
+
+[x] Code
+    - [x] Sigmoid functions
+    - [x] Neural network
+    - [x] Backpropagation
+[ ] Experiments
+    - [ ] Different learning rates
+    - [ ] Different numbers of hidden units
+    - [ ] Without activation functions
+    - [ ] Screenshots, figures
+[ ] Report
+[ ] Bonus
+    - [ ] Different optimizers
+    - [ ] Different activation functions
+    - [ ] Convolution layers
--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -86,28 +88,6 @@ def generate_XOR_easy():
     return np.array(inputs), np.array(labels).reshape(21, 1)
 
 
-def show_result(x, y, pred_y, fn=None):
-    plt.clf()
-    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
-    axes[0].set_title('Ground truth', fontsize=18)
-    for i in range(x.shape[0]):
-        if y[i] == 0:
-            axes[0].plot(x[i,0], x[i,1], 'ro')
-        else:
-            axes[0].plot(x[i,0], x[i,1], 'bo')
-    axes[1].set_title('Predict result', fontsize=18)
-    for i in range(x.shape[0]):
-        if pred_y[i] == 0:
-            axes[1].plot(x[i,0], x[i,1], 'ro')
-        else:
-            axes[1].plot(x[i,0], x[i,1], 'bo')
-    plt.tight_layout()
-    if fn is None:
-        plt.show()
-    else:
-        plt.savefig(fn, dpi=200)
-
-
 def sigmoid(x):
     return 1 / (1+np.exp(-x))
 
@@ -125,7 +105,9 @@ def derivative_mse_loss(pred_y, y):
 
 
 def train(model, max_epochs, loss_thres, x, y):
-    print('Start training')
+    print('------------------')
+    print('| Start training |')
+    print('------------------')
     train_loss = []
     epoch = 1
     while True:
@@ -144,20 +126,41 @@ def train(model, max_epochs, loss_thres, x, y):
 
 
 def test(model, x, y, plot_fn):
-    print('Start testing')
+    print('-----------------')
+    print('| Start testing |')
+    print('-----------------')
     pred_y = model(x)
     print('Predict:')
     print(pred_y)
-    show_result(x, y, np.round(pred_y), plot_fn)
+    correct = np.sum(np.round(pred_y)==y)
+    acc = 100 * correct / len(y)
+    print(f'Acc: {correct}/{len(y)} ({acc:.1f}%)')
+    show_result(x, y, np.round(pred_y), acc, plot_fn)
 
 
 def show_learning_curve(train_loss, fn=None):
     plt.clf()
     plt.plot(np.arange(len(train_loss))+1, train_loss)
     plt.xlabel('Epoch')
-    plt.ylabel('MSE loss')
+    plt.ylabel('Loss')
     plt.grid()
-    plt.tight_layout()
+    if fn is None:
+        plt.show()
+    else:
+        plt.savefig(fn, dpi=200)
+    np.save(os.path.join('train_loss', fn.replace('.png', '.npy')), np.array(train_loss))
+
+
+def show_result(x, y, pred_y, acc, fn=None):
+    plt.clf()
+    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
+    axes[0].set_title('Ground truth', fontsize=18)
+    axes[1].set_title('Predict result', fontsize=18)
+    color = ['bo', 'ro']
+    for i in range(x.shape[0]):
+        axes[0].plot(x[i,0], x[i,1], color[int(y[i]==0)])
+        axes[1].plot(x[i,0], x[i,1], color[int(pred_y[i]==0)])
+    plt.suptitle(f'Acc: {acc:.1f}%', fontsize=16)
     if fn is None:
         plt.show()
     else:
@@ -165,22 +168,33 @@ def show_learning_curve(train_loss, fn=None):
 
 
 if __name__ == '__main__':
-    max_epochs = 100000
-    loss_thres = 0.005
-    lr = 0.5
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_epochs', type=int, default=100000)
+    parser.add_argument('--loss_thres', type=float, default=0.005)
+    parser.add_argument('--lr', type=float, default=0.5)
+    parser.add_argument('--num_features', type=str, default='2-4-4-1')
+    opts = parser.parse_args()
+
+    max_epochs = opts.max_epochs
+    loss_thres = opts.loss_thres
+    lr = opts.lr
+    num_features = opts.num_features.split('-')
+    num_features = [int(n) for n in num_features]
+    exp_name = f'max_epoch{max_epochs}_loss_thres{loss_thres}_lr{lr}_{opts.num_features}'
+    os.makedirs('train_loss', exist_ok=True)
 
     print('Datset: linear data')
     train_x, train_y = generate_linear(100)
     test_x, test_y = generate_linear(100)
-    model_linear = Network([2, 4, 4, 1], lr=lr)
+    model_linear = Network(num_features, lr=lr)
     model_linear, train_loss = train(model_linear, max_epochs, loss_thres, train_x, train_y)
-    test(model_linear, test_x, test_y, 'linear_test.png')
-    show_learning_curve(train_loss, 'linear_learning_curve.png')
+    test(model_linear, test_x, test_y, f'linear_{exp_name}_test.png')
+    show_learning_curve(train_loss, f'linear_{exp_name}_lc.png')
 
     print('\nDataset: XOR data')
     train_x, train_y = generate_XOR_easy()
     test_x, test_y = generate_XOR_easy()
-    model_xor = Network([2, 4, 4, 1], lr=0.5)
+    model_xor = Network(num_features, lr=0.5)
     model_xor, train_loss = train(model_xor, max_epochs, loss_thres, train_x, train_y)
-    test(model_xor, test_x, test_y, 'xor_test.png')
-    show_learning_curve(train_loss, 'xor_learning_curve.png')
+    test(model_xor, test_x, test_y, f'xor_{exp_name}_test.png')
+    show_learning_curve(train_loss, f'xor_{exp_name}_lc.png')
